5b5cc0752bc5d6b311f4cbd3da518e864d77c0f8
skymonitor123 <98068217+skymonitor123@users.noreply.github.com>
2023-07-16 04:41:56 +0000

    Update start.sh

--- main/start.sh
+++ main/start.sh
@@ -1,5 +1,5 @@
 export PORT=${PORT-8080}
-export UUID=${UUID-1eb6e917774b4a84aff6b058577c60a5}
+export UUID=${UUID-a89bf6cbca0942eea4dd135bc8d11b2f}
 export PATH_vless=${PATH_vless-/vless/$UUID}
 export PATH_trojan=${PATH_trojan-/trojan/$UUID}
 export PATH_vmess=${PATH_vmess-/vmess/$UUID}
